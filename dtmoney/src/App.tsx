722d0c3b8d291d697163b4d098e62f6bc125cfaf
Bernardo <bernardo.bt@gmail.com>
2021-11-17 08:57:38 -0300

    feat: movendo o componente Modal de Header p/ App

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,27 +1,50 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard/index";
 import { Header } from "./components/Header/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
-  return (
-    <>
-      <Header />
-      <Dashboard />
-      <GlobalStyle />
-    </>
-  );
+
+	const [isNewTransactionModalOpen,  setIsNewTransactionModalOpen] = useState(false);
+
+	function handleOpenTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+
+	function handleCloseTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+
+	return (
+		<>
+			<Header onOpenTransactionModal={handleOpenTransactionModal} />
+			<Dashboard />
+			<GlobalStyle />
+			<Modal 
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseTransactionModal}
+			>
+				<h2>
+					Alow
+				</h2>
+			</Modal>
+		</>
+	);
 }
 
 // export function traz algumas vantagens
 /*
-  Por que utilizar as exportações com export, ao invés de export default?
-  - no export default, quem define o nome do componente não é quem está exportando
-  e sim quem está importando.
-  - quem exporta somente com 'export' obriga a quem importa a chamar exatamente o
-  nome da função importada. Ele pode até mudar o nome, mas terá que fazer isso 
-  de forma explícita
+Por que utilizar as exportações com export, ao invés de export default?
+- no export default, quem define o nome do componente não é quem está exportando
+e sim quem está importando.
+- quem exporta somente com 'export' obriga a quem importa a chamar exatamente o
+nome da função importada. Ele pode até mudar o nome, mas terá que fazer isso 
+de forma explícita
 
-  Existe uma outra vantagem
-  a importação automática do editor fica mais aprimorada também, já que todo 
-  componente terá obrigatoriamente um nome específico
+Existe uma outra vantagem
+a importação automática do editor fica mais aprimorada também, já que todo 
+componente terá obrigatoriamente um nome específico
 */
--- dtmoney/src/components/Header/Header.tsx
+++ dtmoney/src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logoDTMoney.svg';
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+   onOpenTransactionModal: () => void ;
+}
+
+export function Header(props: HeaderProps) {
+
    return(
       <Container>
          <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">Nova transação</button>
+            <button 
+               type="button"
+               onClick={props.onOpenTransactionModal}
+            >
+               Nova transação
+            </button>
+
+            
          </Content>
       </Container>
    )
