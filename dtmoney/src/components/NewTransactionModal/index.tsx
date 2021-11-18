fc27abddc7ec143df2ed5c39c2cdf5f9bdfee8c1
Bernardo <bernardo.bt@gmail.com>
2021-11-18 10:38:27 -0300

    feat criação doe componentes (TransactionTypeContainer e RadioBox) e gerenciamento de estados

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,18 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import iconClose from '../../assets/close.svg'
+import iconIncome from '../../assets/income.svg'
+import iconOutcome from '../../assets/outcome.svg'
+import { useState } from 'react'
 interface NewTransactionModalProps {
    isOpenModal: boolean;
    onRequestCloseModal: () => void;
 }
 
 export function NewTransactionModal (props: NewTransactionModalProps) {
+   
+   const [type, setType] = useState("")
+
    return(
       <Modal 
 				isOpen={props.isOpenModal}
@@ -35,6 +41,27 @@ export function NewTransactionModal (props: NewTransactionModalProps) {
                type="number"
                placeholder='Título' 
             />
+
+            <TransactionTypeContainer>
+               <RadioBox 
+                  type='button'
+                  onClick={() => {setType('deposit')}}
+                  isActive={type === 'deposit'}
+                  activeColor="green"
+               >
+                  <img src={iconIncome} alt="Entrada" />
+                  <span>Entrada</span>
+               </RadioBox>
+               <RadioBox 
+                  type='button'
+                  onClick={() => {setType('withdraw')}}
+                  isActive={type === 'withdraw'}   
+                  activeColor="red"
+               >
+                  <img src={iconOutcome} alt="Saída" />
+                  <span>Saída</span>
+               </RadioBox>
+            </TransactionTypeContainer>
 
             <input 
                type="text"
