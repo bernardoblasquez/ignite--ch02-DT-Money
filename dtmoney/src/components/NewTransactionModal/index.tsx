546790860a2838509e32fb2c64b72a55dd280a21
Bernardo <bernardo.bt@gmail.com>
2021-11-18 11:10:06 -0300

    feat: salvando dados do formulário.

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,23 +3,42 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import iconClose from '../../assets/close.svg'
 import iconIncome from '../../assets/income.svg'
 import iconOutcome from '../../assets/outcome.svg'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+
 interface NewTransactionModalProps {
    isOpenModal: boolean;
    onRequestCloseModal: () => void;
 }
 
 export function NewTransactionModal (props: NewTransactionModalProps) {
+   const [title, setTitle] = useState("");
+   const [value, setValue] = useState(0);
+   const [category, setCategory] = useState("");
    
-   const [type, setType] = useState("")
+   const [type, setType] = useState("deposit");
+
+   function handleCreateNewTransaction(event: FormEvent) {
+      event.preventDefault();
+      console.log({
+         title,
+         value, 
+         type,
+         category
+      });
+
+      setTitle('');
+      setValue(0);
+      setCategory('');
+      setType('deposit')
+   };
 
    return(
       <Modal 
-				isOpen={props.isOpenModal}
-				onRequestClose={props.onRequestCloseModal}
-            overlayClassName="react-modal-overlay" 
-            className="react-modal-content" 
-			>
+         isOpen={props.isOpenModal}
+         onRequestClose={props.onRequestCloseModal}
+         overlayClassName="react-modal-overlay" 
+         className="react-modal-content" 
+		>
          
          <button 
             className="react-modal-close"
@@ -29,17 +48,21 @@ export function NewTransactionModal (props: NewTransactionModalProps) {
             <img src={iconClose} alt="Fechar Janela" />
          </button>   
 
-         <Container>
+         <Container onSubmit={handleCreateNewTransaction}>
 				<h2> Cadastrar Transações </h2>
 
             <input 
                type="text"
                placeholder='Título' 
+               value={title}
+               onChange={event => setTitle(event.target.value)}
             />
 
             <input 
                type="number"
-               placeholder='Título' 
+               placeholder='Valor'
+               value={value}
+               onChange={event => setValue(Number(event.target.value))} 
             />
 
             <TransactionTypeContainer>
@@ -66,6 +89,8 @@ export function NewTransactionModal (props: NewTransactionModalProps) {
             <input 
                type="text"
                placeholder='Categoria' 
+               value={category}
+               onChange={event => setCategory(event.target.value)}
             />
 
             <button type="submit">Cadastrar</button>
