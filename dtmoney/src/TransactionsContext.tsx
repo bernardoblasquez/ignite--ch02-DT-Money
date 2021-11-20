36f4efef8c34e9e8e7ce3af4967da57e39391163
Bernardo <bernardo.bt@gmail.com>
2021-11-20 12:21:51 -0300

    feat: criação de componente que passa o contexto das Transações

---
+++ dtmoney/src/TransactionsContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "./services/api";
+
+
+interface Transaction {
+   id: number,
+   title: string,
+   type: 'deposit',
+   category: string,
+   amount: number,
+   createdAt: string
+}
+
+interface TransactionsProviderProps {
+   children: ReactNode;
+}
+
+export const TransactionsContext = createContext<Transaction[]>([]);
+
+export function TransactionsProvider(props:TransactionsProviderProps) {
+   
+   const [transactions, setTransactions] = useState<Transaction[]>([]) 
+
+   useEffect(() => {
+      api.get('transactions')
+         .then(response => setTransactions(response.data.transactions))
+   }, [])
+
+   return (
+      <TransactionsContext.Provider value={transactions}>
+         {props.children}
+      </TransactionsContext.Provider>
+   )
+
+}
